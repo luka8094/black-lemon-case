22ad4a5cc6d5d0090e96b673d8778b7bde298f89
luka8094 <luka8094@stud.kea.dk>
2022-04-20 14:05:50 +0200

    refactor+fix: rewrote slider structure and added Desktop-mobile responsiveness to slider.
    style: wrote static setup for slider items.
    
    docs: added comments to script.js for future functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
-console.log("The script is connected.")
+window.onload = () => {
+    console.log("The script is connected.")
 
-//This function will auto-fill the carrousel by appending child elements to it.
-function appendItems(){
-    //TODO: fill functionality
+    //This function will auto-fill the carrousel by appending child elements to it.
+    (function appendItems(){
+        //TODO: fill functionality
+    })()
 }
