8432d3dcf847f8b653e4a1c6647692a57207a30a
luka8094 <luka8094@stud.kea.dk>
2022-04-20 22:18:25 +0200

    fix: bug in functionality of the IIFE from script.js.
    refactor: rewrote styling for the slider section to readjust upon mobile media query size.
    
    style+refactor: segregated relating styling properties to individual stylesheets for maintainability and management.
    
    docs: added comments to stylesheets.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,33 @@
-window.onload = () => {
-    console.log("The script is connected.")
+window.onload= () => {
+    
+    const slider = document.getElementById("slider-row");
+    const sliderItemContent = `
+    <img class="slider-item-image" src="assets/img/image 6.png">
+    <p>HAND POMADE / POMMADE MAINS</p>
+    <p>NOURISHMENT FOR YOUR HANDS</p>
+    <p>159,00 KR</p>
+    `
 
     //This function will auto-fill the carrousel by appending child elements to it.
-    (function appendItems(){
-        //TODO: fill functionality
-    })()
+    //@Params{number}: decides the extra amount of additional suggestion items to be added to the slider 
+    ;(function appendItems(number){
+        for(let i = 0; i < number; i++){
+            const newSliderItem = document.createElement("li")
+
+            newSliderItem.classList.add("slider-item")
+            newSliderItem.innerHTML = sliderItemContent
+
+            slider.append(newSliderItem)
+        }
+    })(3)
+
+    //this function would sanitize suspicious input
+    function sanitizer(text){
+        return text.replace(/</g,'')
+            .replace(/>/g,'')
+            .replace(/=/g, '')
+            .replace(/\\/g,'')
+            .replace(/$/g, '')
+            .replace(/?/g,'')
+    }
 }
