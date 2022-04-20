f4379fdf74acdc2660c7a8ce169ddd88af45b2a0
luka8094 <luka8094@stud.kea.dk>
2022-04-20 10:57:40 +0200

    style: added Desktop-mobile media query responsiveness to the first section as well as rewrote fragments of the product specific styling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
 console.log("The script is connected.")
+
+//This function will auto-fill the carrousel by appending child elements to it.
+function appendItems(){
+    //TODO: fill functionality
+}
