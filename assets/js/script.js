0a5608f7730726a1421e35c31f6996dc80e81564
luka8094 <luka8094@stud.kea.dk>
2022-04-21 13:45:15 +0200

    refactor: removed scaffolding borders and rewrote script.js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 window.onload= () => {
     
     const slider = document.getElementById("slider-row");
-    const sliderItemContent = `
-    <img class="slider-item-image" src="assets/img/image 6.png">
+    const sliderItemContent = `<img class="slider-item-image" src="assets/img/image 6.png">
     <p>HAND POMADE / POMMADE MAINS</p>
     <p>NOURISHMENT FOR YOUR HANDS</p>
     <p>159,00 KR</p>
@@ -15,20 +14,10 @@ window.onload= () => {
             const newSliderItem = document.createElement("li")
 
             newSliderItem.classList.add("slider-item")
-            newSliderItem.innerHTML = sanitizer(sliderItemContent)
+            newSliderItem.innerHTML = sliderItemContent
 
             slider.append(newSliderItem)
         }
     })(5)
 
-    //This function would sanitize suspicious input
-    function sanitizer(input){
-        return input.replace(/</g, '&#60;')
-            .replace(/>/g, '&#62;')
-            .replace(/=/g, '&#61;')
-            .replace(/$/g, '&#36;')
-            .replace(/?/g, '&#63;')
-            .replace(/}/g, '&#125;')
-            .replace(/{/g, '&#123;')
-    }
 }
